b668c52365ce83c720fe929c564ba70bfe02c25f
Remaii <remaii1819@gmail.com>
2024-06-19 10:15:52 +0200

    slug.service

---
+++ src/enigme/slug.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SlugService {
+  static generateSlug(str: string) {
+    return str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+  }
+}
