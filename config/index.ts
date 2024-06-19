0c3e7b43c87d6be527d7f0e59dc9c2e1f5556db7
Remaii <remaii1819@gmail.com>
2024-06-19 10:16:47 +0200

    config: expose architecture

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,12 @@
 import config from './config';
 
 export default {
-  ...config,
+  mongo: {
+    uri: config.mongo.uri,
+    user: config.mongo.user,
+    pwd: config.mongo.pwd,
+  },
+  jwt: {
+    secretKey: config.jwt.secretKey,
+  },
 };
