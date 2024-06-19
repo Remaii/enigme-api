dd11f9955852863565f68f5acd7aeb20d9cc1e41
Remaii <remaii1819@gmail.com>
2024-06-19 12:15:06 +0200

    app.module: add activity private endpoints and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { AuthMiddleware } from './auth/auth.middleware';
 import { AuthModule } from './auth/auth.module';
 import { EnigmeModule } from './enigme/enigme.module';
+import { ActivityModule } from './activity/activity.module';
 import config from '../config';
 
 const fullMongoUri =
@@ -21,22 +22,28 @@ const fullMongoUri =
     UserModule,
     AuthModule,
     EnigmeModule,
+    ActivityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .forRoutes(
-        { path: 'users', method: RequestMethod.GET },
-        { path: 'users/me', method: RequestMethod.GET },
-        { path: 'users/:id', method: RequestMethod.GET },
-        { path: 'users/:id', method: RequestMethod.PATCH },
-        { path: 'users/:id', method: RequestMethod.DELETE },
-        { path: 'enigmes', method: RequestMethod.POST },
-        { path: 'enigmes/:id', method: RequestMethod.PATCH },
-      );
+    consumer.apply(AuthMiddleware).forRoutes(
+      // Users
+      { path: 'users', method: RequestMethod.GET },
+      { path: 'users/me', method: RequestMethod.GET },
+      { path: 'users/:id', method: RequestMethod.GET },
+      { path: 'users/:id', method: RequestMethod.PATCH },
+      { path: 'users/:id', method: RequestMethod.DELETE },
+      // Enigmes
+      { path: 'enigmes', method: RequestMethod.POST },
+      { path: 'enigmes/:id', method: RequestMethod.PATCH },
+      { path: 'enigmes/:id', method: RequestMethod.DELETE },
+      // Activity
+      { path: 'activity', method: RequestMethod.POST },
+      { path: 'activity/:id', method: RequestMethod.PATCH },
+      { path: 'activity/:id', method: RequestMethod.DELETE },
+    );
   }
 }
